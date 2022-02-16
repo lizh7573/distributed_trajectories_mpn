c30f485b0c6e4aaa6c0b01f5d64d99252ed1a7b6
Liyuan Zhang <69516244+lizh7573@users.noreply.github.com>
2022-02-16 05:26:03 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    author='Liyuan Zhang',
+    author_email='liyuan.zhang.7573@student.uu.se',
+    name='distributed_trajectories_mpn',
+    version='0.1.0',
+    install_requires=[],
+    license='Apache License',
+    packages=find_packages(include=['distributed_trajectories_mpn', 'distributed_trajectories_mpn.*']),
+    include_package_data=True,
+    setup_requires=['pytest_runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    url='https://github.com/lizh7573/distributed_trajectories_mpn.git',
+)
